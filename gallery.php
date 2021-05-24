85a731fbf917c1919129f8c683c0585660265ee1
Joseph Kelechi <77107025+jo-kelechi@users.noreply.github.com>
2021-05-24 19:37:59 +0100

    header image
    Changed the header image for other web pages apart from index

--- gallery.php
+++ gallery.php
@@ -48,13 +48,34 @@
     </div>
 
     <div class="inner-content">
+      <!--Navegation Start-->
+      <!-- <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container"> <a class="navbar-brand" href="#">Menu</a>
+          <div class="navbar-dark">
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+          </div>
+          <div class="collapse navbar-collapse" id="myBtnContainer">
+            <ul class="navbar-nav mr-auto" id="navbarhdr">
+              <li class="nav-item active"> <a class="nav-link" href="index.php">Home</a></li>
+              <li class="nav-item"> <a class="nav-link" href="about.php">About</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="admission.php">Admission</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="curriculum.php">Curriculum</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="form.php"> forms</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="gallery.php">Gallery</a> </li>
+              <li class="nav-item"> <a class="nav-link" href="contact.php">Contact</a> </li>
+            </ul>
+          </div>
+        </div>
+      </nav> -->
+      
+      <!--Navegation End--> 
       <div id="myBtnContainer">
-        <button class="btn active" onclick="filterSelection('all')"> Show all</button>
-        <button class="btn" onclick="filterSelection('class1')"> Class One</button>
-        <button class="btn" onclick="filterSelection('class2')"> Class Two</button>
-        <button class="btn" onclick="filterSelection('class3')"> Class Three</button>
-        <button class="btn" onclick="filterSelection('class4')"> Class Four</button>
-        <button class="btn" onclick="filterSelection('kinder')"> Kindergarten</button>
+        <a class="btn active" onclick="filterSelection('all')"> Show all</a>
+        <a class="btn" onclick="filterSelection('class1')"> Class One</a>
+        <a class="btn" onclick="filterSelection('class2')"> Class Two</a>
+        <a class="btn" onclick="filterSelection('class3')"> Class Three</a>
+        <a class="btn" onclick="filterSelection('class4')"> Class Four</a>
+        <a class="btn" onclick="filterSelection('kinder')"> Kindergarten</a>
       </div>
       <div class="container">
         <div class="galleryWrp">
